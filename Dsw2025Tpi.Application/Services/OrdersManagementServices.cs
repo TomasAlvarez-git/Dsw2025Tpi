409551e8c1894f6645e66da29c5ec65e94557487
TomasAlvarez-git <Tomas.AlvarezPaez@alu.frt.utn.edu.ar>
2025-06-22 20:04:32 -0300

    Se trabajo sobre la funcionalidad de crear una orden

---
+++ Dsw2025Tpi.Application/Services/OrdersManagementServices.cs
@@ -0,0 +1,19 @@
+﻿using Dsw2025Tpi.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Tpi.Application.Services
+{
+    public class OrdersManagementService
+    {
+        private readonly IRepository _repository;
+
+        public OrdersManagementService(IRepository repository)
+        {
+            _repository = repository;
+        }
+    }
+}
