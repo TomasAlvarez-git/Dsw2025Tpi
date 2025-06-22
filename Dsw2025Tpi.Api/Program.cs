fdf175db7fe73f7df6e47be789ae28c53eb05a75
TomasAlvarez-git <Tomas.AlvarezPaez@alu.frt.utn.edu.ar>
2025-06-22 19:09:20 -0300

    Se realizo la conexion con la BD completando las clases context, program y contextExtension

--- Dsw2025Tpi.Api/Program.cs
+++ Dsw2025Tpi.Api/Program.cs
@@ -1,4 +1,12 @@
 
+using Dsw2025Tpi.Application.Services;
+using Dsw2025Tpi.Data;
+using Dsw2025Tpi.Data.Helpers;
+using Dsw2025Tpi.Data.Repositories;
+using Dsw2025Tpi.Domain.Entities;
+using Dsw2025Tpi.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 namespace Dsw2025Tpi.Api;
 
 public class Program
@@ -14,6 +22,19 @@ public class Program
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
         builder.Services.AddHealthChecks();
+        builder.Services.AddDbContext<Dsw2025TpiContext>(options =>
+        {
+            options.UseSqlServer(builder.Configuration.GetConnectionString("Dsw2025TpiEntities"));
+            options.UseSeeding((c, t) =>
+            {
+                ((Dsw2025TpiContext)c).Seedwork<Order>("Sources\\orders.json");
+                ((Dsw2025TpiContext)c).Seedwork<Product>("Sources\\products.json");
+                ((Dsw2025TpiContext)c).Seedwork<OrderItem>("Sources\\orderitems.json");
+                ((Dsw2025TpiContext)c).Seedwork<Customer>("Sources\\customers.json");
+            });
+        });
+        builder.Services.AddScoped<IRepository, EfRepository>();
+        builder.Services.AddTransient<ProductsManagementService>();
 
         var app = builder.Build();
 
--- Dsw2025Tpi.Data/Dsw2025TpiContext.cs
+++ Dsw2025Tpi.Data/Dsw2025TpiContext.cs
@@ -1,7 +1,56 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Dsw2025Tpi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Dsw2025Tpi.Data;
 
-public class Dsw2025TpiContext: DbContext
+public class Dsw2025TpiContext : DbContext
 {
+
+
+    public Dsw2025TpiContext(DbContextOptions<Dsw2025TpiContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Product>(p =>
+        {
+            p.HasKey(p => p.Id);
+            p.Property(p => p.Sku).IsRequired().HasMaxLength(15);
+            p.Property(p => p.Name).IsRequired().HasMaxLength(60);
+            p.Property(p => p.InternalCode).HasMaxLength(30);
+            p.Property(p => p.Description).HasMaxLength(500);
+            p.Property(p => p.CurrentPrice).IsRequired();
+            p.Property(p => p.StockQuantity).HasPrecision(15, 2).IsRequired();
+            p.Property(p => p.IsActive).IsRequired().HasDefaultValue(true);
+        });
+
+        modelBuilder.Entity<Order>(o =>
+        {
+            o.HasKey(o => o.Id);
+            o.Property(o => o.Date).IsRequired();
+            o.Property(o => o.ShipingAddress).IsRequired().HasMaxLength(200);
+            o.Property(o => o.BillingAddress).IsRequired().HasMaxLength(200);
+            o.Property(o => o.Notes).HasMaxLength(500);
+            o.Property(o => o.TotalAmount).IsRequired().HasPrecision(15, 2);
+        });
+
+        modelBuilder.Entity<OrderItem>(oi =>
+        {
+            oi.HasKey(oi => oi.Id);
+            oi.Property(oi => oi.Quantity).IsRequired();
+            oi.Property(oi => oi.UnitPrice).IsRequired().HasPrecision(15, 2);
+        });
+
+        modelBuilder.Entity<Customer>(c =>
+        {
+            c.HasKey(c => c.Id);
+            c.Property(c => c.Id).IsRequired().ValueGeneratedOnAdd();
+            c.Property(c => c.Name).IsRequired().HasMaxLength(60);
+            c.Property(c => c.Email).IsRequired().HasMaxLength(100);
+            c.Property(c => c.Phone).HasMaxLength(15);
+        });
+    }
+
 }
