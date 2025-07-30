9d9eefabcd39d7a5368edcab6c63c23d340404ca
TomasAlvarez-git <Tomas.AlvarezPaez@alu.frt.utn.edu.ar>
2025-07-30 19:12:14 -0300

    Se agregaron las clases para las excepciones

---
+++ Dsw2025Tpi.Api/Middleware/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Dsw2025Tpi.Api.Middleware
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionMiddleware(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<ExceptionMiddleware>();
+        }
+    }
+}
